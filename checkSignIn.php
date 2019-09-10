c23b04f7e6e7cb2e2dd6ae6bf42d754f2c0e87b4
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-10 23:37:52 +0300

    use session to send messages / activate remember me button

--- checkSignIn.php
+++ checkSignIn.php
@@ -1,26 +1,34 @@
 <?php
 include 'connectToDB.php';
+session_start();
 $email = $_POST['email'];
 
 if($result=$con->query("SELECT * FROM users WHERE email='$_POST[email]'")) {
   $row = $result->fetch_array(MYSQLI_ASSOC);
   if($_POST['password']!='') {
     if(password_verify($_POST['password'],$row['password'])){
-      setcookie('email', $email, time() + (86400 * 30), "/"); // 86400 = 1 day
-      setcookie('first_name', $row['first_name'], time() + (86400 * 30), "/");
+      if(isset($_POST['remember'])) {
+        setcookie('email', $email, time() + (86400 * 30), "/"); // 86400 = 1 day
+        setcookie('first_name', $row['first_name'], time() + (86400 * 30), "/");
+      }
+      $_SESSION['email']=$email;
+      $_SESSION['first_name']=$row['first_name'];
       header('Location:./index.php');
     }
     else {
-      header('Location:./login.php?error=Wrong Username/Password');
+      $_SESSION['error'] = 'Wrong Username/Password';
+      header('Location:./login.php');
     }
 
   }
   else {
-    header('Location:./login.php?error=Enter Username and Password');
+    $_SESSION['error'] = 'Enter Username and Password';
+    header('Location:./login.php');
   }
 }
 else {
-  header('Location:./login.php?error=Wrong Username/Password');
+  $_SESSION['error'] = 'Wrong Username/Password';
+  header('Location:./login.php');
 }
 
 
