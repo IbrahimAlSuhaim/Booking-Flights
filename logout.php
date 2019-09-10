3c26d7caf9e4c41483f79b1d4b3469b123689593
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-10 23:41:41 +0300

    delete cookies or session

--- logout.php
+++ logout.php
@@ -1,5 +1,27 @@
 <?php
-// set the expiration date to one hour ago
-setcookie("email", "", time() - (86400 * 30),"/");
-header('Location:./login.php?message=See you later.')
+  include './preventUnregisteredUsers.php';
+ ?>
+
+<?php
+session_start();
+
+if (isset($_COOKIE['email'])) { // is user using cookies?
+  $first_name = $_COOKIE['first_name'];
+  setcookie("email", "", time() - (86400 * 30),"/");
+  setcookie("first_name", "", time() - (86400 * 30),"/");
+  $_SESSION['message']='See you later '.$first_name;
+  header('Location:./login.php');
+}
+else { // or session
+  $first_name = $_SESSION['first_name'];
+  session_unset();
+  session_destroy();// destroys all of the data associated with the current session.
+  session_start();
+  $_SESSION['message']='See you later '.$first_name;
+  header('Location:./login.php');
+}
+
+
+
+
 ?>
