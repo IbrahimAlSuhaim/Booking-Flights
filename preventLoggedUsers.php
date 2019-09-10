e555ab51078b36ff124271ae9bab49f264a44afa
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-10 23:42:06 +0300

    prevent logged in users from entering the page

---
+++ preventLoggedUsers.php
@@ -0,0 +1,8 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+if(isset($_COOKIE['email'])||isset($_SESSION['email'])){ //prevent logged in users from entering this page
+  header('location:./index.php');
+}
+ ?>
