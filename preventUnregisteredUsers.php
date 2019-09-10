a73b1e22b77c59ce3ef3f3d7a77413bdad0659a2
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-10 23:42:22 +0300

    prevent unregistered users from entering the page

---
+++ preventUnregisteredUsers.php
@@ -0,0 +1,8 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+if(!isset($_COOKIE['email'])&&!isset($_SESSION['email'])){ //prevent unregistered users from entering this page
+  header('location:./index.php');
+}
+ ?>
