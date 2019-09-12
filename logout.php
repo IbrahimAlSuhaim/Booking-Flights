2f901c164672ac7cb9d249d6b90bc4bfb920d1e1
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-12 15:32:13 +0300

    fix cookie logout. proplem: signout button needs to be clicked two times

--- logout.php
+++ logout.php
@@ -2,21 +2,16 @@
 session_start();
 include './preventUnregisteredUsers.php';
 
-if (isset($_COOKIE['email'])) { // is user using cookies?
-  $first_name = $_COOKIE['first_name'];
-  setcookie("email", "", time() - (86400 * 30),"/");
-  setcookie("first_name", "", time() - (86400 * 30),"/");
-  $_SESSION['message']='See you later '.$first_name;
-  header('Location:./login.php');
-}
-else { // or session
-  $first_name = $_SESSION['first_name'];
-  session_unset();
-  session_destroy();// destroys all of the data associated with the current session.
-  session_start();
-  $_SESSION['message']='See you later '.$first_name;
-  header('Location:./login.php');
-}
+$first_name = $_COOKIE['first_name']? $_COOKIE['first_name'] : $_SESSION['first_name'];
+setcookie("email", "", time() - 3600,"/");
+setcookie("first_name", "", time() - 3600,"/");
+
+session_unset();
+session_destroy();// destroys all of the data associated with the current session.
+session_start();
+$_SESSION['message']='See you later '.$first_name;
+header('Location:./login.php');
+
 
 
 
