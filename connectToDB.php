dc0eb9f2f3002eda36dca5baf80f3cc51a5dba9e
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-13 02:33:12 +0300

    heroku conn DB

--- connectToDB.php
+++ connectToDB.php
@@ -1,7 +1,16 @@
 <?php
 // enter your sql login information------
 
-$con=mysqli_connect("localhost","root","","booking_flight");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
+
 if(mysqli_connect_errno()){
   die("Couldn't connect to the database beacuse: ".mysqli_connect_error());
 }
