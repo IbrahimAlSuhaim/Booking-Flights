fc9b866fa47e001453f72fda9ef36417c077f5cb
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-12 15:05:37 +0300

    add exit()

--- preventLoggedUsers.php
+++ preventLoggedUsers.php
@@ -4,5 +4,6 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 if(isset($_COOKIE['email'])||isset($_SESSION['email'])){ //prevent logged in users from entering this page
   header('location:./index.php');
+  exit();
 }
  ?>
