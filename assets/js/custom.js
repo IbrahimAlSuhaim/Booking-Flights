c9f161b970196a7938ba5e0517058930ce0e74e4
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-27 23:17:18 +0300

    delete value of departure_date when click one way

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $('input:radio[name="directionality"]').change(
-    function(){
-        if (this.checked && this.value == 'oneWay') {
-          $('#return_picker').attr('hidden', "true");
-          $('#return_picker').attr('required', "false");
-          $('#datepicker_label').html('Departure');
-        }
-        else {
-          $('#return_picker').removeAttr('hidden');
-          $('#return_picker').attr('required', "true");
-          $('#datepicker_label').html('Departure - Return');
-        }
+      function(){
+      if (this.checked && this.value == 'oneWay') {
+        $('#return_picker').attr('hidden', "true");
+        $('#return_picker').attr('required', "false");
+        $('#datepicker_label').html('Departure');
+        $('#return_picker').val('');
+        $('#departure_date').val('');
+      }
+      else {
+        $('#return_picker').removeAttr('hidden');
+        $('#return_picker').attr('required', "true");
+        $('#datepicker_label').html('Departure - Return');
+      }
     });
 });
