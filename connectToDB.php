660dde65d81add20a0b13a9e87085c6ea3b8a9f3
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-27 16:09:50 +0300

    edit connection to DB file

--- connectToDB.php
+++ connectToDB.php
@@ -1,18 +1,22 @@
 <?php
 // enter your sql login information------
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$con = new mysqli($server, $username, $password, $db);
-
+$con=mysqli_connect("localhost","root","","booking_flight");
 
 if(mysqli_connect_errno()){
-  die("Couldn't connect to the database beacuse: ".mysqli_connect_error());
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $con = new mysqli($server, $username, $password, $db);
+
+
+  if(mysqli_connect_errno()){
+    die("Couldn't connect to the database beacuse: ".mysqli_connect_error());
+  }
 }
 
 
