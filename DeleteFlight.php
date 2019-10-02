0215d0143a927a69be2b74a31155b92ae3f6f726
IbrahimAlSuhaim <ka20@windowslive.com>
2019-10-02 13:58:20 +0300

    edit delete flight

--- DeleteFlight.php
+++ DeleteFlight.php
@@ -2,25 +2,36 @@
 
 include './connectToDB.php';
 
-if(empty($_POST['flight_number']))
-{
-    
-  header('Location:dashboard.php?failMsg=You must fill the FLIGHT_NUMBER');
+// if(empty($_POST['flight_number']))
+// {
+//
+//   header('Location:dashboard.php?failMsg=You must fill the FLIGHT_NUMBER');
+//     exit();
+// }
+//
+// if(!isset($_POST['submit']))
+// {
+//     header('Location:dashboard.php?failMsg=All fields are required');
+//     exit();
+// }
+$del = $_GET['flight_number'];
+
+if(mysqli_query($con,"SELECT flight_number FROM `flights` WHERE `flight_number` = '$del'")){
+  $sql = " DELETE FROM `flights` WHERE `flight_number` = '$del'";
+  if(mysqli_query($con,$sql)){
+    header("Location:dashboard.php?successMsg='$del' has been deleted successfully");
     exit();
+  }
+  else {
+    header("Location:dashboard.php?failMsg=".mysqli_error($con));
+    exit();
+  }
+}
+else {
+  header("Location:dashboard.php?failMsg='$del' not in the database");
+  exit();
 }
 
-if(!isset($_POST['submit']))
-{
-    header('Location:dashboard.php?failMsg=All fields are required');
-    exit();
-}
-$del = $_POST['flight_number'];
-
-$sql = " DELETE FROM `flights` WHERE `flight_number` = '$del'";
-
-mysqli_query($con,$sql);
-
-header('Location:dashboard.php?successMsg=Deleted successfully');
 
 mysqli_close($con);
 
