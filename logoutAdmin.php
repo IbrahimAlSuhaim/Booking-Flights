4e99b0444714a96c22cbc0b72e77b6503cb2e652
IbrahimAlSuhaim <ka20@windowslive.com>
2019-10-02 09:51:21 +0300

    fix logout admin cookies

--- logoutAdmin.php
+++ logoutAdmin.php
@@ -1,18 +1,11 @@
 <?php
 session_start();
 
-$first_name = $_COOKIE['first_name']? $_COOKIE['first_name'] : $_SESSION['first_name'];
-setcookie("email", "", time() - 3600,"/");
-setcookie("first_name", "", time() - 3600,"/");
+setcookie("username", "", time() - 3600,"/");
 
 session_unset();
 session_destroy();// destroys all of the data associated with the current session.
 session_start();
-$_SESSION['message']='See you later ';
+$_SESSION['message']='See you later sir!';
 header('Location:./admin');
-
-
-
-
-
 ?>
