09481b782269036fa23a40d44cbb3a7c1b0b943a
IbrahimAlSuhaim <ka20@windowslive.com>
2019-10-23 12:59:55 +0300

    add confirmation email

--- bookflight.php
+++ bookflight.php
@@ -6,6 +6,19 @@ $passengersNum = $_SESSION['departure_flight']['passengers'];
 $departure_flight_id = $_SESSION['departure_flight_id'];
 $class = $_SESSION['departure_flight']['class'];
 
+if(isset($_COOKIE['user_id'])) {
+  $user_id = $_COOKIE['user_id'];
+}
+else if(isset($_SESSION['user_id'])) {
+  $user_id = $_SESSION['user_id'];
+}
+else {
+  $_SESSION['next'] = './bookflight';
+  $_SESSION['error'] = 'Please login to continue or register';
+  header('Location:./login');
+  exit();
+}
+
 for ($i=1; $i <= $passengersNum ; $i++) {
   $first_name = $listPassengers["first_name_$i"]; $middle_name = $listPassengers["middle_name_$i"]; $family_name = $listPassengers["family_name_$i"]; $nationality = $listPassengers["nationality_$i"]; $document_type = $listPassengers["document_type_$i"]; $document_number = $listPassengers["document_number_$i"];
   $birth_date = $listPassengers["birth_date_$i"]; $gender = $listPassengers["gender_$i"]; $number = $listPassengers["number_$i"]; $email = $listPassengers["email_$i"];
@@ -23,18 +36,6 @@ for ($i=1; $i <= $passengersNum ; $i++) {
     // todo: update passenger information
   }
 
-  if(isset($_COOKIE['user_id'])) {
-    $user_id = $_COOKIE['user_id'];
-  }
-  else if(isset($_SESSION['user_id'])) {
-    $user_id = $_SESSION['user_id'];
-  }
-  else {
-    $_SESSION['next'] = './bookflight';
-    $_SESSION['error'] = 'Please login to continue or register';
-    header('Location:./login');
-    exit();
-  }
   $sql = "SELECT `passenger_id` FROM `passengers` WHERE `document_type`='$document_type' AND `document_number`= '$document_number'";
   $result = $con->query($sql);
   $row=mysqli_fetch_array($result);
@@ -58,8 +59,30 @@ for ($i=1; $i <= $passengersNum ; $i++) {
       incReserved($con, $return_flight_id);
     }
   }
-  header("Location:./confirmation");
 }
+$result = $con->query("SELECT * FROM users WHERE user_id=$user_id");
+$row=mysqli_fetch_array($result);
+$emailTo=$row['email'];
+
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("No-replay@bookflights.com", "Bookflights Team");
+$email->setSubject("Thank you for booking");
+$email->addTo($emailTo, $first_name);
+$email->addContent("text/plain", "Thank you for booking, ".$first_name."Your flight booking is confirmed");
+$email->addContent(
+    "text/html", "<p>Thank you for booking, ".$first_name."</p><br><strong>Your flight booking is confirmed</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
+header("Location:./confirmation");
 
 
 function incReserved($con, $flight_id) {
