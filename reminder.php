299659f95abe0f027976403aa9f5d4899e4ff1fc
unknown <i.su7aim@gmail.com>
2019-12-03 21:23:12 +0300

    update reminder

--- reminder.php
+++ reminder.php
@@ -1,33 +1,43 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+require 'vendor/autoload.php';
 
-$emailTo= 'i.su7aim@gmail.com';
-$user_first_name = 'Ibrahim';
+include './connectToDB.php';
+$sql = "SELECT users.email AS user_email, users.first_name AS user_first_name, passengers.first_name, passengers.family_name, flights.departure_date, flights.departure_time, booked_tickets.ticket_id FROM users, flights, booked_tickets, passengers
+WHERE users.user_id = booked_tickets.user_id AND flights.flight_id = booked_tickets.flight_id AND passengers.passenger_id = booked_tickets.passenger_id
+AND flights.departure_date <= ".date('Y-m-d',strtotime("+1 days"))." AND flights.departure_time < '".date("H:i")."' AND booked_tickets.emailSent = 0 ";
+$result=$con->query($sql);
+if($result->num_rows>0) {
+  while($row=mysqli_fetch_array($result)) {
+    $emailTo = $row['user_email'];
+    $user_first_name = $row['user_first_name'];
 
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("No-reply@bookflights.com", "Bookflights Team");
+    $email->setSubject("Reminder");
+    $email->addTo($emailTo, $user_first_name);
+    $email->addContent("text/plain", "Hi, ".$user_first_name." This is a flight reminder");
+    $email->addContent(
+        "text/html", "<p>Hello ". $user_first_name. " 👋</p>
+        <br>
+        <strong>You have a flight in ".$row['departure_date']." at ".$row['departure_time']." with passenger name: ".$row['first_name']." ".$row['family_name']."</strong>
+        <br>
+        <br>
+        <p>✈️ for more info visit </p><a href='http://book-flights.herokuapp.com/'> http://book-flights.herokuapp.com/</a>"
+    );
+    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    try {
+        $response = $sendgrid->send($email);
+        print $response->statusCode() . "\n";
+        print_r($response->headers());
+        print $response->body() . "\n";
+    } catch (Exception $e) {
+        echo 'Caught exception: '. $e->getMessage() ."\n";
+    }
 
-$email = new \SendGrid\Mail\Mail();
-$email->setFrom("No-reply@bookflights.com", "Bookflights Team");
-$email->setSubject("Reminder");
-$email->addTo($emailTo, $user_first_name);
-$email->addContent("text/plain", "Hi, ".$user_first_name." This is a reminder");
-$email->addContent(
-    "text/html", "<p>Hi 👋, ".$user_first_name."</p><br><strong>This is a reminder ⏰</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+    $sql = "UPDATE booked_tickets SET emailSent = 1 WHERE ticket_id ='$row[ticket_id]'";
+    $con->query($sql);
+  }
 }
+
 
  ?>
